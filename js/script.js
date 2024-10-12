ccf299ad8e38ef2fb372bbab5b3070b90cdc1abe
talleresviviryaprender <talleresviviryaprender@gmail.com>
2024-10-12 19:36:40 -0300

    Update script.js
    mas cambios

--- js/script.js
+++ js/script.js
@@ -6,10 +6,3 @@ document.addEventListener('DOMContentLoaded', function() {
         mainNavbar.classList.toggle('active');
     });
 });
-
-const balloon = document.getElementById('floatingBalloon');
-        const buttons = document.getElementById('socialButtons');
-
-        balloon.addEventListener('click', () => {
-            buttons.style.display = buttons.style.display === 'none' || buttons.style.display === '' ? 'flex' : 'none';
-        });
